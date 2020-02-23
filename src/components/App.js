74c97e94631d7924a3a0702e8e5ab4208b52b308
mortizo <mortizo@ups.edu.ec>
2020-02-23 21:03:28 +0100

    Cambia la URL

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="http://www.ups.edu.ec"
             target="_blank"
             rel="noopener noreferrer"
           >
